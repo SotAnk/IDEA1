26d6a932cce85d20d683505833c0187c8353c736
Anna Sotnikova <ruceba77@gmail.com>
2021-10-24 19:24:52 +0600

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main
 {
     public static void main (String[] args) {
-                int currentBalance = 200000;
-                int balanceAmount = 500000;
+                int currentBalance = 2000000000;
+                int balanceAmount = 500000000;
                 int totalAmount = currentBalance + balanceAmount;
                 System.out.println("Total: " +totalAmount);
 
