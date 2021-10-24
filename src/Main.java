398b1083bfef00e3d6a689062dd5caa4cbd07de2
Anna Sotnikova <ruceba77@gmail.com>
2021-10-24 18:59:21 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main (String[] args) {
+                int currentBalance = 200000;
+                int balanceAmount = 500000;
+                int totalAmount = currentBalance + balanceAmount;
+                System.out.println("Total: " +totalAmount);
+
+    }
+
+
+
+}
